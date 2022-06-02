4182f0424ffe6180a752e2ea722cc60a74e2e5a6
Pollyana <pollyanarocha416@gmail.com>
2022-06-02 19:34:16 -0300

    Typecast

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Aula10
+{
+    static void Main()
+    {
+        //convercao do tipo inplicita
+        int n1 = 10;
+        float n2 = n1;
+        Console.WriteLine(n2);//10
+
+        //typecast  CONVERCAO é necessário quando nao e segura ou seja Esplicita
+        float n3 = 10.5f;
+        int n4 = (int)n3;
+        Console.WriteLine(n4);//10
+
+        double a = 23.7;
+        int n = (int)a;
+        Console.WriteLine(n);
+
+        double p = 43948.5;
+        float q =  (float)p;
+
+
+
+    }
+}
